edb8cecad701e1f4000bf45499ef034f1b3ec0d7
glag <glaguyonie@gmail.com>
2025-12-10 22:03:51 +0100

    add scripts for server and worker

---
+++ p1/scripts/provisionSW.sh
@@ -0,0 +1,5 @@
+while [ ! -f /vagrant/node-token ]; do sleep 2; done  # wait for server token
+
+TOKEN=$(cat /vagrant/node-token)
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -s - --node-ip=192.168.56.111 #use the token to connect to master
+sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.111/24 dev eth1 && sudo ip link set eth1 up #same trick
