4241093b99a565de8ff49a2da4b9f6badc4ac969
glag <glaguyonie@gmail.com>
2025-12-11 20:14:50 +0100

    me forgor some lines

--- p2/scripts/provisionS.sh
+++ p2/scripts/provisionS.sh
@@ -1,1 +1,3 @@
 curl -sfL https://get.k3s.io | sh -s - --node-ip=192.168.56.110 #use the vm private network
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token #create token for workers
+sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up #safeguard for apps that use eth1
