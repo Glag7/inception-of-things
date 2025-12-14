1989aed89d882fd396d902497dc7c8fb666c1739
glag <glaguyonie@gmail.com>
2025-12-14 17:51:34 +0100

    add script start.sh

---
+++ p3/scripts/start.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "creating k3d cluster"
+k3d cluster create iot \ #cluster name for commands
+  --servers 1 \
+  --agents 2 \ # n of agents, >= 2 for load balancing
+  --port "8888:8888@loadbalancer" \      # wil's app uses port 888
+  					#load balancer manages traffic between nodes
+  --port "80:80@loadbalancer" \          # argocd ui http
+  --port "443:443@loadbalancer" \        # https
+  --api-port 6443 #needed to control kubernetes cluster (kubectl, argocd, etc.)
+
+echo "cluster created. waiting for nodes"
+kubectl wait --for=condition=Ready nodes --all --timeout=120s
+
+echo "creating namespaces"
+kubectl create namespace argocd
+kubectl create namespace dev
+
+echo "installing argocd in 'argocd' namespace"
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "waiting for argocd pods to be ready"
+kubectl wait --for=condition=Ready pods --all -n argocd --timeout=300s
+
+echo "=== argocd credentials ==="
+echo "login: admin"
+echo -n "password: "
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) #password in is base64, decode it
+echo "$ARGOCD_PASSWORD"
+echo "$ARGOCD_PASSWORD" > pass.txt
+
+echo "forwarding argocd ui port on 8080"
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+
+while ! nc -z localhost 8080; do   #while port inst forwarded wait
+  sleep 1
+done
+
+echo "setting up argo"
+
+#log in
+argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
+
+#argo will monitor this repo
+argocd repo add https://github.com/Glag7/inception-of-things-test-app-glaguyon.git
+argocd app create wil-playground \ #name in argocd ui
+  --repo https://github.com/Glag7/inception-of-things-test-app-glaguyon.git \ #repo to monitor
+  --path manifest/app \ #path in repo
+  --dest-server https://kubernetes.default.svc \ #use current cluster
+  --dest-namespace dev \ #deploy to dev namespace
+  --project default \ #simple config for demo
+  --sync-policy automated #auto sync
