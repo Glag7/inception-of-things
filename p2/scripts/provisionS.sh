928b0177f3a88e76720fb6c5303b3940d28558ab
a <glaguyonie@gmail.com>
2025-12-16 21:15:40 +0100

    change provision to use correct path

--- p2/scripts/provisionS.sh
+++ p2/scripts/provisionS.sh
@@ -3,9 +3,9 @@ sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token #create to
 sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up #safeguard for apps that use eth1
 
 #deploy + service
-kubectl apply -f /vagrant/apps/app1.yaml
-kubectl apply -f /vagrant/apps/app2.yaml
-kubectl apply -f /vagrant/apps/app3.yaml
+kubectl apply -f /vagrant/confs/app1.yaml
+kubectl apply -f /vagrant/confs/app2.yaml
+kubectl apply -f /vagrant/confs/app3.yaml
 
 #ingress (creates domain names)
-kubectl apply -f /vagrant/apps/appingress.yaml
+kubectl apply -f /vagrant/confs/appingress.yaml
